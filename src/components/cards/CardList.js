260f8d656b13ccc84d5c4fe343ef0f58aa9fc0ac
mmeacham92 <meacham.michael@rocketmail.com>
2021-03-09 12:00:33 -0600

    changed cardList from a ul to a table; card__table-div will now only show if a set has been selected

--- src/components/cards/CardList.js
+++ src/components/cards/CardList.js
@@ -1,13 +1,39 @@
 const CardList = ({ cardList, currentSet }) => {
+  if (cardList.length === 0) return '';
   return (
-    <div className="cards__list">
+    <div className="card__table-div">
       <h1>Current Set: {currentSet.name}</h1>
-      <ul>
-        {cardList.map(card => {
-            return <li key={card.id}>{card.name}</li>
-        })}
-      </ul>
+      <table className='card__table'>
+        <thead>
+          <tr>
+            <th>No.</th>
+            <th>Card Name</th>
+            <th>Type</th>
+            <th>Rarity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cardList.map(card => {
+            return (
+              <tr key={card.id}>
+                <td>{card.number}</td>
+                <td>{card.name}</td>
+                <td>{card.types ? card.types[0] : ''}</td>
+                <td>{card.rarity}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </div>
+    // <div className="cards__list">
+    //   <h1>Current Set: {currentSet.name}</h1>
+    //   <ul class='card__list-item'>
+    //     {cardList.map(card => {
+    //         return <li key={card.id}>{card.name}</li>
+    //     })}
+    //   </ul>
+    // </div>
   );
 };
 
